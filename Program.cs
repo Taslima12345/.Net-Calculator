58ef1bb993839b0055108cc3ac714045237b5cd7
Taslima12345 <35329197+Taslima12345@users.noreply.github.com>
2020-01-20 17:40:33 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        public double[] takeingInput()
+        {
+            string numer;
+            int value;
+            
+            numer = Console.ReadLine();
+            value = Convert.ToInt32(numer);
+
+            double[] ele = new double[value];
+
+            for (int i=0; i<value; value++)
+            {
+                ele[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            return ele;
+        }
+         
+
+
+    }
+}
+
+  
+
